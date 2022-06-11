9dae01b131defbf4e0b3d184f987793c77f75cd6
Felipealvesnn <felipe.alvesnn@gmail.com>
2022-06-10 22:57:55 -0300

    add

---
+++ index.js
@@ -0,0 +1,31 @@
+function somar(numero1, numero2) {
+    let soma = numero1 + numero2;
+    return `A soma dos dois números é: ${soma}`;
+}
+
+const resultadoSoma = somar(2, 3);
+console.log(resultadoSoma);
+
+function dividisao(numero1, numero2) {
+    let divisao = numero1 / numero2;
+    return `A divisao dos dois números é: ${divisao}`;
+}
+
+const resultadodividir = dividisao(10, 5);
+console.log(resultadodividir);
+
+function subtracao(numero1, numero2) {
+    let menos = numero1 - numero2;
+    return `A subtracao dos dois números é: ${menos}`;
+}
+
+const resultadosubtracao = subtracao(10, 9);
+console.log(resultadosubtracao);
+
+function multiplicacao(numero1, numero2) {
+    let multiplicar = numero1 * numero2;
+    return `A multiplicacao dos dois números é: ${multiplicar}`;
+}
+
+const resultadomultiplicacao = multiplicacao(10, 5);
+console.log(resultadomultiplicacao);
